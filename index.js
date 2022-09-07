b463cb02f1f6a37d31f2bb271c6f17206b07491d
Jasmine Chamblin <jasminesimone.jc@gmail.com>
2022-09-06 17:16:36 -0700

    altered index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
-const inquirer = require ('inquirer')
-const fs = require ('fs')
+const inquirer = import ('inquirer')
+const fs = import ('fs')
 // TODO: Create an array of questions for user input
 
 inquirer.prompt([
